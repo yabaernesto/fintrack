1373df47d3bfa1f9f8d0d5b0c8585c4f06ef8a2c
Yaba Ernesto <yabaernesto@gmail.com>
2026-05-17 13:25:23 +0100

    chore: add sort imports plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error',
     },
   },
 ]
